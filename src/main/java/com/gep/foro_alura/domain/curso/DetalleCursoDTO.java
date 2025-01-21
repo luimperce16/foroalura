0fdf992d7c04fd4a6dd76cbf5e3eb36a7caacf3b
luimperce16 <luimperce@gmail.com>
2025-01-21 10:19:59 -0500

    se agrega el Proyecto

---
+++ src/main/java/com/gep/foro_alura/domain/curso/DetalleCursoDTO.java
@@ -0,0 +1,12 @@
+package com.gep.foro_alura.domain.curso;
+
+
+public record DetalleCursoDTO(Long id, String name, Categoria categoria, Boolean activo) {//Define los parametros a recibir
+
+    //Constructor
+    public DetalleCursoDTO(Curso curso){
+        this(curso.getId(), curso.getName(), curso.getCategoria(), curso.getActivo());
+    }
+}
+
+
